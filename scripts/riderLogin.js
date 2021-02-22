70e3a98648ce65c5fe87640c32ed7aef6297c348
itsmejayvee0818 <42688683+itsmejayvee0818@users.noreply.github.com>
2021-02-22 16:26:35 +0800

    added designs

---
+++ scripts/riderLogin.js
@@ -0,0 +1,20 @@
+(()=>{
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyD4fhbxQpNdc64uJ631OqrfpqqoCoKDhvU",
+        authDomain: "emporiumdavao-68ce4.firebaseapp.com",
+        projectId: "emporiumdavao-68ce4",
+        storageBucket: "emporiumdavao-68ce4.appspot.com",
+        messagingSenderId: "611634768650",
+        appId: "1:611634768650:web:2411919c5b64947ceea8f3"
+    };
+      // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    $(document).ready(function(){
+        $('.slider').slider({
+            indicators: false
+        });
+    });
+
+})();
