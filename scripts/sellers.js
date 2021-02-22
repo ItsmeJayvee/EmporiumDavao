fa99cc87afb62848f1fa3e485438f83edbcbc037
itsmejayvee0818 <42688683+itsmejayvee0818@users.noreply.github.com>
2021-02-22 15:00:45 +0800

    added script tags

--- scripts/sellers.js
+++ scripts/sellers.js
@@ -1,4 +1,16 @@
 (()=>{
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyD4fhbxQpNdc64uJ631OqrfpqqoCoKDhvU",
+        authDomain: "emporiumdavao-68ce4.firebaseapp.com",
+        projectId: "emporiumdavao-68ce4",
+        storageBucket: "emporiumdavao-68ce4.appspot.com",
+        messagingSenderId: "611634768650",
+        appId: "1:611634768650:web:2411919c5b64947ceea8f3"
+    };
+      // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
     $(document).ready(function(){
         $('.sidenav').sidenav();
     });
